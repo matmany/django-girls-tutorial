b1b20a1c0d37225e992ed4a028e0186a08427c70
matmany <matmanymat@gmail.com>
2020-10-16 10:48:17 -0300

    documentando views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -51,17 +51,60 @@ def post_draft_list(request):
 
 @login_required
 def post_publish(request, pk):
+    """
+    Redireciona ao `post_detail`, Após a publicação.
+
+    Para publicar é necessario estar logado. 
+    Umas vez publicado é redirecionado ao `blog/post_detail.html`
+
+    **Context**
+
+    ``pk``
+        Uma Key de :model:`blog.Post`.
+    
+    **Templates:**
+
+    :template:`blog/post_detail.html`
+    """
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
 @login_required
 def post_remove(request, pk):
+    """
+   Deleta um Post e Redireciona ao `post_detail`. 
+
+   Delete o Post selecionado pela `pk`.
+    **Templates:**
+
+    :template:`blog/post_list.html`
+    """
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
 def add_comment_to_post(request, pk):
+    """
+    Exibe formulario do `blog.forms.CommentForm` ou Redireciona ao `post_detail`.
+
+    O formulario blog/add_comment_to_post.html é exibido. \n 
+    Caso haja um requisito POST, o formulario é validado e salvo.
+    
+    **Context**
+
+    ``form``
+        Uma instancia `blog.forms.CommentForm`
+
+    ``pk``
+        Uma Key de :model:`blog.Post`.
+    
+    **Templates:**
+
+    :template:`blog/add_comment_to_post.html`
+
+    :template:`blog/post_detail.html`
+    """
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
@@ -75,11 +118,35 @@ def add_comment_to_post(request, pk):
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 def comment_remove(request, pk):
+    """
+    Exibe Detalhes de um :model:`blog.Post` quando comentario é removido.
+
+    **Context**
+
+    ``pk``
+        Uma Key de :model:`blog.Post`.
+    
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
 def comment_approve(request, pk):
+    """
+    Redireciona para `post_detail` quando comentario é aprovado
+
+    **Context**
+
+    ``pk``
+        Uma Key de :model:`blog.Post`.
+    
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
