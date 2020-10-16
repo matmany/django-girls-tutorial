43804c9044fa004653851001e4b4f39fbdd162ae
JenniferAmaral <jennifergamaral@gmail.com>
2020-10-16 09:33:04 -0300

    documentando as views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,15 +8,55 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
+    """
+    Essa funcao exibe uma lista de posts marcados
+    como publicados e ordenados pela data da publicacao.
+
+    **Context**
+
+    ``post``
+       Uma instancia :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_list.html`
+    """
+    
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request,'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
+    """
+    Essa funcao exibe os detalhes de um post
+
+    **Context**
+
+    ``post``
+       Uma instancia :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     post = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html', {'post': post})
 
 @login_required
 def post_new(request):
+    """
+    Essa funcao registra o conteúdo de um novo post
+
+    **Context**
+
+    ``post``
+       Uma instancia :model:`blog.Post`.
+
+    ``form``
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -31,6 +71,18 @@ def post_new(request):
 
 @login_required
 def post_edit(request, pk):
+    """
+    Essa funcao registra e atualiza as modificações do conteudo de um post existente
+
+    **Context**
+
+    ``post``
+       Uma instancia :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_edit.html`
+    """
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -46,6 +98,20 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
+    """
+    Essa funcao exibe uma lista de posts não publicados/ sem data de publicacao
+
+    **Context**
+
+    ``post``
+       Uma instancia :model:`blog.Post`.
+
+    ``form``
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     posts = Post.objects.filter(publish_date__isnull=True).order_by('created_date')
     return render(request,'blog/post_draft_list.html', {'posts':posts})
 
@@ -83,3 +149,4 @@ def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
